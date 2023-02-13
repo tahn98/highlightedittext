2d7d17d9eace1f05b38d40e39e5c9152f93317db
nhattq98 <nhattran@vulcanlabs.co>
2023-02-13 18:01:47 +0700

    second commit

--- app/src/main/java/com/example/myapplication/HighlightEditText.kt
+++ app/src/main/java/com/example/myapplication/HighlightEditText.kt
@@ -1,16 +1,51 @@
 package com.example.myapplication
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Typeface
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.BackgroundColorSpan
+import android.text.style.StyleSpan
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.widget.FrameLayout
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatEditText
 import com.example.myapplication.databinding.LayoutHighlightEdtBinding
 
+/**
+ * EditText to support highlight key
+ */
 class HighlightEditText(context: Context, attrs: AttributeSet?, defStyle: Int) :
-    AppCompatEditText(context, attrs, defStyle) {
+    FrameLayout(context, attrs, defStyle) {
+
+    private var keywordList = arrayListOf<String>("python", "Say Hello")
+
     constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0)
     constructor(context: Context) : this(context, null, 0)
 
     private var binding: LayoutHighlightEdtBinding =
-        LayoutHighlightEdtBinding.inflate(LayoutInflater.from(context), null, true)
+        LayoutHighlightEdtBinding.inflate(LayoutInflater.from(context), this, true)
+    private val editText: AppCompatEditText = binding.edt
+
+    init {
+        binding.edt.setText("Generate Python code for Say Hello", TextView.BufferType.EDITABLE)
+        spannable()
+    }
+
+
+    private fun spannable(){
+        val boldSpan = StyleSpan(Typeface.BOLD)
+        val start: Int = 2
+        val end: Int = 4
+        val flag = Spannable.SPAN_INCLUSIVE_INCLUSIVE
+        editText.text?.setSpan(boldSpan, start, end, flag)
+        editText.text?.setSpan(BackgroundColorSpan(Color.BLUE),2, 4, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+    }
+
+    private fun background(){
+    }
+
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<FrameLayout>(R.id.ed_highlight)
     }
 }
